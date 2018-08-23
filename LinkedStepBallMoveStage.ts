6f7bc0e9721c733b85c7b9176702f31f6fb659fc
Anwesh43 <anweshthecool0@gmail.com>
2018-08-23 11:17:31 +0530

    adding StepBallMover class

--- LinkedStepBallMoveStage.ts
+++ LinkedStepBallMoveStage.ts
@@ -141,3 +141,25 @@ class SMNode {
 			return this
 		}
 }
+
+class StepBallMover {
+	curr : SMNode = new SMNode(0)
+	dir : number = 1
+
+	draw(context : CanvasRenderingContext2D) {
+		this.curr.draw(context, (i) => this.curr.i == i)
+	}
+
+	update(cb : Function) {
+		this.curr.update(() => {
+			this.curr = this.curr.getNext(this.dir, () => {
+				this.dir *= -1
+			})
+			cb()
+		})
+	}
+
+	startUpdating(cb : Function) {
+		this.curr.startUpdating(cb)
+	}
+}
